3c7eb844baa587a2c41031a36733cb9762ba7368
+ <+>
2024-11-28 20:38:41 +0300

    recreates js fns in python

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
-def greet(name):
-    pass
+def greet(name= "Shaline"):
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    #print(num1 + num2)
+    return num1 + num2
+    #add(45, 55)
 
 def halve(number):
-    pass
+    #print (number/2)
+    return (number/2)
+    #halve(100)
